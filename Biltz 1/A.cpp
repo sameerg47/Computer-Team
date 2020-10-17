abcc73171eb8d5a322da7adfeb5104049b628b43
Sameer Gabbita <57297991+sameerg47@users.noreply.github.com>
2020-10-16 22:54:23 -0400

    Update A.cpp

--- Biltz 1/A.cpp
+++ Biltz 1/A.cpp
@@ -3,20 +3,13 @@ using namespace std;
 
 int main() {
    cin.tie(0)->sync_with_stdio(false);
-   int n;
-   cin >> n;
+   int n; cin >> n;
    unordered_map<int, int> m;
    int res = 0;
    while (n--) {
-      int t;
-      cin >> t;
-      if (m.find(t) == m.end()) {
-         m[t] = 1;
-         res = max(res, m[t]);
-      } else {
-         m[t]++;
-         res = max(res, m[t]);
-      }
+      int t; cin >> t;
+      m[t]++;
+      res = max(res, m[t]);
    }
    cout << res;
 }
