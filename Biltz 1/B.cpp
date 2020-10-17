980909a6ca4f6986eeb1c6c6e07dbf9a61952eb9
Sameer Gabbita <57297991+sameerg47@users.noreply.github.com>
2020-10-16 22:55:04 -0400

    Update B.cpp

--- Biltz 1/B.cpp
+++ Biltz 1/B.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main() {
    cin.tie(0)->sync_with_stdio(false);
-   int n, a[100001];
+   int n, a[100000];
    cin >> n;
    for (int i = 0; i < n; ++i) cin >> a[i];
    sort(a, a+n);
    int ans = 0;
-   for (int i = 1; i < n; i+=2) ans += a[i];
+   for (int i = 1; i < n; i += 2) ans += a[i];
    cout << ans;
 }
