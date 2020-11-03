8408842f94e6169ccbd278daa2e4e64e9a4c9c12
Sameer Gabbita <57297991+sameerg47@users.noreply.github.com>
2020-11-02 21:10:07 -0500

    Update iceCreamTruck.cpp

--- mBIT 2020/iceCreamTruck.cpp
+++ mBIT 2020/iceCreamTruck.cpp
@@ -1,16 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-
 int main() {
-   cin.tie(0)->sync_with_stdio(false);
-   
-   int a, b, n;
-   cin >> a >> b >> n;
-   
-   if (5*a > b) {
-      cout << (n/5)*b + (n%b)*a;
-   } else {
-      cout << n*a;
-   }
+    int a, b, n; cin >> a >> b >> n;
+    if (b < 5 * a) cout << n / 5 * b + n % 5 * a;
+    else cout << n * a;
 }
