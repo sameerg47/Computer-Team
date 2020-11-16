cac6ee09951cedbd87b113a5523a8008aae60c39
Sameer Gabbita <57297991+sameerg47@users.noreply.github.com>
2020-11-15 22:34:40 -0500

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using vi=vector<int>;
+using pi=pair<int, int>;
+
+#define FOR(i, a, b) for(int i=(a); i<(b); ++i)
+#define FORZ(i, a) for(int i=0; i<(a); ++i)
+#define ROF(i, a, b) for(int i=a; i>=b; --i)
+#define ROFZ(i, a) for(int i=a; i>=0; --i)
+#define EACH(i, a) for(auto &i:a)
+
+#define pb push_back
+#define pf push_front
+#define eb emplace_back
+#define lb lower_bound
+#define ub upper_bound
+#define mp make_pair
+#define f first
+#define s second
+#define rsz resize
+#define sz size
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+}
